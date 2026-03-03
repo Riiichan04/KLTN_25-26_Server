6a53c0ce3f80c72ad4c0ad59206092cf1bf25142
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2026-03-03 22:17:20 +0700

    Config spring boot project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.1.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "vn.id.nonglam.kltn"
+version = "0.0.1-SNAPSHOT"
+description = "Graduation project server"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/vn/id/nonglam/kltn/kltn/KltnApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.id.nonglam.kltn.kltn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KltnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

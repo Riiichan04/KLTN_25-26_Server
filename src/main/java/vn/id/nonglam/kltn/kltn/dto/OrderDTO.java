9a84e4dd7225076ab7bbfe50b04c31ee8a285ec8
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2026-03-14 18:39:28 +0700

    feat(booking): add create order method

--- src/main/java/vn/id/nonglam/kltn/kltn/dto/OrderDTO.java
+++ src/main/java/vn/id/nonglam/kltn/kltn/dto/OrderDTO.java
@@ -1,13 +1,16 @@
 package vn.id.nonglam.kltn.kltn.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public class OrderDTO {
+    public record OrderRequest(List<UUID> roomDetailsId, String note, LocalDateTime checkin, LocalDateTime checkout) {}
+
     public record RoomDetailSnapShotResponse(UUID id, String code, boolean valid) {}
 
     public record RoomDetailValidResponse(UUID roomTypeId, String name,
                                           List<RoomDetailSnapShotResponse> data) {}
 
-    public record OrderResponse() {}
+    public record OrderResponse(boolean status, UUID orderId, double deposit) {}
 }
--- src/main/java/vn/id/nonglam/kltn/kltn/repositories/RoomDetailRepository.java
+++ src/main/java/vn/id/nonglam/kltn/kltn/repositories/RoomDetailRepository.java
@@ -1,5 +1,6 @@
 package vn.id.nonglam.kltn.kltn.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.id.nonglam.kltn.kltn.models.hotel.RoomDetail;
@@ -8,5 +9,7 @@ import java.util.UUID;
 
 @Repository
 public interface RoomDetailRepository extends JpaRepository<RoomDetail, UUID> {
-
+    boolean existsByIdAndActiveTrue(UUID id);
+    @EntityGraph(attributePaths = {"roomType"})
+    RoomDetail findByIdAndActiveTrue(UUID id);
 }

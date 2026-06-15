803f0885fa2f4eefc1264e845c985f532cef8abc
Riichan04 <22130274@st.hcmuaf.edu.vn>
2026-06-16 02:27:24 +0700

    fix: fix response not group by in statistic query

--- src/main/java/vn/id/nonglam/kltn/kltn/repositories/AddressRepository.java
+++ src/main/java/vn/id/nonglam/kltn/kltn/repositories/AddressRepository.java
@@ -3,8 +3,8 @@ package vn.id.nonglam.kltn.kltn.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import vn.id.nonglam.kltn.kltn.dto.response.HomePageStatisticResponse;
 import vn.id.nonglam.kltn.kltn.models.hotel.Address;
-import vn.id.nonglam.kltn.kltn.dto.response.HomePageStatisticResponse.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,14 +13,15 @@ import java.util.UUID;
 public interface AddressRepository extends JpaRepository<Address, UUID> {
     //Will be added order by booking completed count by province
     @Query("""
-        select
-            a.province as name,
-            count(a.province) as hotelCount,
-            a.postalCode as postalCode,
-            h.thumbnail as thumbnail
-        from Address a join Hotel h on a.id = h.address.id
-        group by a.province, a.postalCode, h.thumbnail
-        order by count(a.province)
-    """)
-    List<ProvinceStatistic> statisticAddress();
+                select new vn.id.nonglam.kltn.kltn.dto.response.HomePageStatisticResponse$ProvinceStatistic(
+                    a.province,
+                    count(a.province),
+                    a.postalCode,
+                    MAX(h.thumbnail)
+                )
+                from Address a join Hotel h on a.id = h.address.id
+                group by a.province, a.postalCode
+                order by count(a.province) DESC
+            """)
+    List<HomePageStatisticResponse.ProvinceStatistic> statisticAddress();
 }

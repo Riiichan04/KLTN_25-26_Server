c32f22a77070257946b76e531d5297321f19210e
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2026-07-31 01:08:54 +0700

    feat(admin): complete order management api

---
+++ src/main/java/vn/id/nonglam/kltn/kltn/controllers/admin/OrderManagementController.java
@@ -0,0 +1,40 @@
+package vn.id.nonglam.kltn.kltn.controllers.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vn.id.nonglam.kltn.kltn.dto.request.admin.ChangeOrderStatusRequest;
+import vn.id.nonglam.kltn.kltn.dto.response.admin.AdminOrderResponse;
+import vn.id.nonglam.kltn.kltn.services.admin.AdminOrderService;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/orders")
+@RequiredArgsConstructor
+public class OrderManagementController {
+    private final AdminOrderService adminOrderService;
+
+    @GetMapping("/get")
+    public ResponseEntity<Page<AdminOrderResponse>> getOrders(@RequestParam(required = false, defaultValue = "") String keyword,
+                                                              @RequestParam(required = false) LocalDateTime startDate,
+                                                              @RequestParam(required = false) LocalDateTime endDate,
+                                                              @PageableDefault(
+                                     size = 10,
+                                     sort = "createdAt",
+                                     direction = Sort.Direction.DESC
+                             ) Pageable pageable) {
+        return ResponseEntity.ok(adminOrderService.getOrders(keyword, startDate, endDate, pageable));
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Boolean> changeStatusOrder(@PathVariable UUID id, @RequestBody ChangeOrderStatusRequest req) {
+        return ResponseEntity.ok(adminOrderService.changeStatus(id, req));
+    }
+
+}
--- src/main/java/vn/id/nonglam/kltn/kltn/repositories/OrderRepository.java
+++ src/main/java/vn/id/nonglam/kltn/kltn/repositories/OrderRepository.java
@@ -2,6 +2,7 @@ package vn.id.nonglam.kltn.kltn.repositories;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,10 +33,21 @@ public interface OrderRepository extends JpaRepository<Order, UUID> {
             @Param("endDate") LocalDateTime endDate,
             @Param("status") OrderStatus status
     );
+
     Page<Order> findByUser_IdAndOrderStatus(UUID userId, OrderStatus status, Pageable pageable);
 
     @Query("SELECT o.orderStatus AS status, COUNT(o) AS count FROM Order o WHERE o.user.id = :userId GROUP BY o.orderStatus")
     List<OrderStatusCount> countOrderStatusByUserId(@Param("userId") UUID userId);
 
     List<Order> findByUser_Id(UUID user_id);
+
+    @Query("""
+        select o from Order o left join o.hotel h
+        where (h is null or h.name ilike concat('%', :keyword, '%'))
+        and (:startDate is null or o.createdAt >= :startDate)
+        and (:endDate is null or o.createdAt <= :endDate)
+    """)
+    @EntityGraph(attributePaths = {"hotel", "hotel.owner", "user"})
+    Page<Order> findAll(@Param("keyword") String keyword, @Param("startDate") LocalDateTime startDate,
+                        @Param("endDate") LocalDateTime endDate, Pageable pageable);
 }

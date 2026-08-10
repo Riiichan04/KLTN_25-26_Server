f05f5287c3d6b5cbae854739607449f0a30f8b86
Riichan04 <22130274@st.hcmuaf.edu.vn>
2026-08-10 23:11:33 +0700

    debug: debug error when getting hotel

--- src/main/java/vn/id/nonglam/kltn/kltn/security/SecurityConfig.java
+++ src/main/java/vn/id/nonglam/kltn/kltn/security/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                         .requestMatchers("/booking/choose-room").permitAll()
                         .requestMatchers("/admin/**").hasRole(UserRole.ADMIN.name())
                         .requestMatchers("/owner/**").hasRole(UserRole.OWNER.name())
+                        .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
